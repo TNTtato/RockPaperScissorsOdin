1a480d91b0a21b282f6603c06fc5d7773febc410
gustavo <penarandagustavo5@gmail.com>
2022-04-23 17:48:45 -0500

    getting rid of commented code

--- script.js
+++ script.js
@@ -4,16 +4,7 @@ function computerPlay() {
   let computerChoice = computerOptions[randIndex];
   return computerChoice;
 }
-/*
-function userPlay() {
-  let userChoice = prompt("Input your choice: ");
-  let lowerChoice = userChoice.toLowerCase();
-  while (lowerChoice !== "rock" && lowerChoice !== "paper" && lowerChoice !== "scissors") {
-    userChoice = prompt("Input a correct choice: ");
-    lowerChoice = userChoice.toLowerCase();
-  }
-  return lowerChoice;
-}*/
+
 function playRound(computerChoice, userChoice) {
   let winnerRound = "";
   let compWinning = computerChoice === "rock" && userChoice === "scissors" || computerChoice === "scissors" && userChoice === "paper" || computerChoice === "paper" && userChoice === "rock";
@@ -30,51 +21,7 @@ function playRound(computerChoice, userChoice) {
   }
   return winnerRound;
 }
-/*
-function game(computer, human) {
-  let counterComputer = 0,
-    counterUser = 0;
-    let compSel = computer;
-    let userSel = human;
-    let roundResult = playRound(compSel, userSel);
-    let condWinnig = roundResult.charAt(0);
-    if (condWinnig === "C") {
-      counterComputer++;
-    }
-    if (condWinnig === "Y") {
-      counterUser++;
-    }
-    console.log(roundResult);
-    if (counterComputer >= 5) {
-      console.log('computer won');
-    }
-    if (counterUser >= 5) {
-      console.log('user won');
-    }
-}
 
-*/
-/*
-let rock = document.getElementById('rock');
-let paper = document.getElementById('paper');
-let scissors = document.getElementById('scissors');
-
-rock.addEventListener('click', function() {
-  let userChoice = 'rock';
-  let round = playRound(computerPlay(), userChoice);
-  console.log(round);
-});
-paper.addEventListener('click', function() {
-  let userChoice = 'paper';
-  let round = playRound(computerPlay(), userChoice);
-  console.log(round);
-});
-scissors.addEventListener('click', function() {
-  let userChoice = 'scissors';
-  let round = playRound(computerPlay(), userChoice);
-  console.log(round);
-});
-*/
 const choices = document.querySelectorAll('.choice');
 let contUser = 0;
 let contComp = 0;
